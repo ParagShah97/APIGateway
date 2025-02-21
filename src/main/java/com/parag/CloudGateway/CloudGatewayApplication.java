4a85c41347ee0eb92f7ac04f3e1ab3dfd2dcddce
ParagShah97 <parag.shah97@gmail.com>
2025-02-20 19:28:54 -0500

    Added Redis, circuit-break and route for APIs

---
+++ src/main/java/com/parag/CloudGateway/CloudGatewayApplication.java
@@ -0,0 +1,36 @@
+package com.parag.CloudGateway;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+	// Return KeyResolver instance: for rate limiter purpose.
+	@Bean
+	KeyResolver userKeyResolver() {
+		// Currently we don't have users so only one generic key "userKey" is
+		// specified. Later we will fix that.
+		return exchange -> Mono.just("userKey");
+	}
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(
+				id -> new Resilience4JConfigBuilder(id)
+						.circuitBreakerConfig(
+								CircuitBreakerConfig.ofDefaults()
+						).build()
+		);
+	}
+}
